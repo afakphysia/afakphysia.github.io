a0a6df77fcae5a3e94d4c00a2ccf18268f44727d
afakphysia <arpsmagazine@gmail.com>
2024-02-25 20:03:15 +0100

    So 25. Feb 20:03:12 CET 2024

---
+++ ghost-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+date=$(date)
+git pull origin master
+rm -r docs
+mkdir docs
+cd docs
+echo "https://abshiro-azhar.github.io" > CNAME
+cd -
+ECTO1_SOURCE=http://localhost:2368 ECTO1_TARGET=https://abshiro-azhar.github.io python3 ecto1.py
+cd docs
+docker cp ghost:/var/lib/ghost/content/images/. content/images
+cd -
+grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
+git add .
+git commit -m "$date"
+git config --global credential.helper store
+git push -u origin master
